68c4d647332dbf47d93f377c01a648c877412058
Joshua A. Díaz Robayna <jadrdevcm@gmail.com>
2020-07-01 18:36:57 +0100

    Página de inicio de sesión

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from 'react-native';
+import {validateEmail} from "../utils/validation"
+import firebase from "../utils/firebase"
 
 export default function LoginForm(props) {
     const { changeForm } = props
+    const login = () => {
+        console.log('Iniciado Sesión')
+    }
 
     return (
-        <View>
-            <Text>LoginForm</Text>
-            <TouchableOpacity>
-                <Text style={styles.btnText} onPress={changeForm}>
-                    Registrate
-                </Text>
+        <>
+         <TextInput 
+            style={styles.input}
+            placeholder="Correo Electronico"
+            placeholderTextColor="#969696"
+         />
+
+        <TextInput 
+            style={styles.input}
+            placeholder="Contraseña"
+            placeholderTextColor="#969696"
+            secureTextEntry={true}
+         />
+
+         <TouchableOpacity onPress={login}>
+             <Text style={styles.btnText}>
+             Iniciar Sesión
+             </Text>
+         </TouchableOpacity>     
+
+            <TouchableOpacity onPress={changeForm}>
+                <Text style={styles.btnText}>Registrate</Text>
             </TouchableOpacity>
-        </View>
+        </>
     );
 }
 
@@ -20,5 +41,17 @@ const styles = StyleSheet.create({
     btnText: {
         color: '#fff',
         fontSize: 18,
+    },
+    input: {
+        height: 50,
+        color: '#fff',
+        width: '80%',
+        marginBottom: 25,
+        backgroundColor: '#1e3040',
+        paddingHorizontal: 20,
+        borderRadius: 50,
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: '#1e3040',
     },
 });
