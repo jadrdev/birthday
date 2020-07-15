adc1ac5ca56bc58d064365f6d297a18af547f149
Joshua A. Díaz Robayna <jadrdevcm@gmail.com>
2020-07-15 17:45:32 +0100

    Update ListBirthday
    Añadida Función de Formatear Fechas
    Corección de error de orderBy

--- src/components/Listbirthday.js
+++ src/components/Listbirthday.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import moment from 'moment'
+import { Text, StyleSheet, View, ScrollView } from 'react-native'
 import AddBirthday from '../components/AddBirthday'
 import ActionBar from '../components/ActionBar'
+import Birthday from '../components/Birthday'
 import firebase from '../utils/firebase'
 import 'firebase/firestore'
 
@@ -11,12 +13,17 @@ const db = firebase.firestore(firebase)
 export default function Listbirthday(props) {
     const { user } = props
     const [showList, SetShowList] = useState(true)
-    const [birhday, setBirhday] = useState({})
+    const [birthday, setBirthday] = useState({})
+    const [pasaBirthday, setpasaBirthday] = useState([])
+    const [reloadData, setreloadData] = useState(false)
+
+    //console.log(birthday)
 
     useEffect(() => {
-        setBirhday([])
+        setBirthday([])
+        setpasaBirthday([])
         db.collection(user.uid)
-            .orderBy('dateBith', 'asc')
+            .orderBy('datebirth', 'asc')
             .get()
             .then(response => {
                 const itemArray = []
@@ -25,27 +32,62 @@ export default function Listbirthday(props) {
                     data.id = doc.id
                     itemArray.push(data)
                 })
-                console.log(itemArray)
+                formatData(itemArray)
             })
-    }, [])
+        setreloadData(false)
+    }, [reloadData, user.uid])
 
+    // Función para formatear fechas
+    const formatData = items => {
+        const currentDate = moment().set({
+            hour: 0,
+            minute: 0,
+            second: 0,
+            millisecond: 0,
+        })
+        const birthdayTempArray = []
+        const pasabirthdayTempArray = []
+
+        items.forEach(item => {
+            const dateBirth = new Date(item.datebirth.seconds * 1000)
+            const dateBirthday = moment(dateBirth)
+            const currentYear = moment().get('year')
+            dateBirthday.set({ year: currentYear })
+
+            const diffDate = currentDate.diff(dateBirthday, 'days')
+            const itemTemp = item
+            itemTemp.dateBirthday = dateBirthday
+            itemTemp.days = diffDate
+
+            if (diffDate <= 0) {
+                birthdayTempArray.push(itemTemp)
+            } else {
+                pasabirthdayTempArray.push(itemTemp)
+            }
+        })
+        //Comprobación de los array llehgan bien
+        //console.log(birthdayTempArray)
+        //console.log(pasabirthdayTempArray)
+        setBirthday(birthdayTempArray)
+        setpasaBirthday(pasabirthdayTempArray)
+    }
     return (
         <View style={styles.container}>
             {showList ? (
-                <>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                    <Text> Listbirthday </Text>
-                </>
+                <ScrollView style={styles.scrollview}>
+                    {birthday.map((item, index) => (
+                        <Birthday key={index} birthday={item} />
+                    ))}
+                    {pasaBirthday.map((item, index) => (
+                        <Birthday key={index} birthday={item} />
+                    ))}
+                </ScrollView>
             ) : (
-                <AddBirthday user={user} SetShowList={SetShowList} />
+                <AddBirthday
+                    user={user}
+                    SetShowList={SetShowList}
+                    setreloadData={setreloadData}
+                />
             )}
             <ActionBar showList={showList} SetShowList={SetShowList} />
         </View>
@@ -56,5 +98,9 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         height: '100%',
+    },
+    scrollview: {
+        marginBottom: 50,
+        width: '100%',
     },
 })
